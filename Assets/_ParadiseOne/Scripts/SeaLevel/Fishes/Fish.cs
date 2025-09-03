cd30338bc4883ae555b92772a59f524982c92f31
Haiito92 <hannaantoinebusiness@gmail.com>
2025-09-03 22:48:46 +0200

    [GP] Spawn Fish List + Fish Lifetime

--- Assets/_ParadiseOne/Scripts/SeaLevel/Fishes/Fish.cs
+++ Assets/_ParadiseOne/Scripts/SeaLevel/Fishes/Fish.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 public abstract class Fish : MonoBehaviour, IFish
 {
     #region Properties
+    [Header("References")]
+    [SerializeField] private SeaTimer _fishLifeTimer;
+    
+    [Header("Fish Stats")]
     [field: SerializeField] public int Score { get; private set; } = 10;
+
+    [SerializeField] private float _fishLifeTime = 10;
+    
     #endregion
 
     #region Actions
     public event Action<PlayerEnum,int> FishCollected;
+    public event Action<Fish> FishDied; //Passes self reference (more easy, I don't want to do index or whatever)
     #endregion
     
     public virtual void CollectFish(PlayerEnum collector)
     {
         FishCollected?.Invoke(collector, Score);
+        
+        KillFish();
+    }
+
+    public void StartFishLife()
+    {
+        _fishLifeTimer.InitTimer(_fishLifeTime);
+        _fishLifeTimer.SeaTimerElapsed += OnFishLifeTimerElapsed;
+        
+        
+        _fishLifeTimer.StartTimer();
+    }
+
+    public void KillFish()
+    {
+        FishDied?.Invoke(this);
+        
+        Destroy(this.gameObject);
+    }
+
+    private void OnFishLifeTimerElapsed()
+    {
+        KillFish();
     }
 }
--- Assets/_ParadiseOne/Scripts/SeaLevel/Fishes/SeaFishSpawner.cs
+++ Assets/_ParadiseOne/Scripts/SeaLevel/Fishes/SeaFishSpawner.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SeaFishSpawner : MonoBehaviour
 {
     #region Fields
     
-    private List<Fish> _fishes;
+    private List<Fish> _fishes = new List<Fish>();
 
     //Spawning
     private Camera _seaLevelCamera;
@@ -15,7 +16,8 @@ public class SeaFishSpawner : MonoBehaviour
     [SerializeField] private SeaTimer _spawningTimer;
     [SerializeField] private float _spawningInterval = 5;
     [SerializeField] private int _maxNumberOfFish;
-    
+
+    [SerializeField] private GameObject _oursinPrefab;
     #endregion
 
     #region Actions
@@ -24,7 +26,7 @@ public class SeaFishSpawner : MonoBehaviour
 
     #endregion
 
-    #region Spawner
+    #region Spawner LifeCycle
 
     private void SetupSpawningZone()
     {
@@ -63,19 +65,70 @@ public class SeaFishSpawner : MonoBehaviour
 
     public void StopSpawner()
     {
-        //TODO
+        _spawningTimer.StopTimer();
+        
+        for (int i = 0; i < _fishes.Count; )
+        {
+            _fishes[i].KillFish();
+        }
     }
     #endregion
 
+    #region SpawnManagement
+
+    
+
+    private void SpawnFish()
+    {
+        if(_fishes.Count >= _maxNumberOfFish) return;
+        
+        Debug.LogWarning("SpawnFish");
+        
+        //Find random point on map
+        float minX = _spawningZone.center.x - _spawningZone.size.x / 2;
+        float maxX = _spawningZone.center.x + _spawningZone.size.x / 2;
+        float spawningX = Random.Range(minX, maxX);
+        
+        float minY = _spawningZone.center.y - _spawningZone.size.y / 2;
+        float maxY = _spawningZone.center.y + _spawningZone.size.y / 2;
+        float spawningY = Random.Range(minY, maxY);
+
+        Vector2 spawningPosition = new Vector2(spawningX, spawningY);
+        
+        //Spawn Fish (first with prefab)
+
+        GameObject fishGO = GameObject.Instantiate(_oursinPrefab, spawningPosition, this.gameObject.transform.rotation, this.transform);
+        Fish fish = fishGO.GetComponent<Fish>();
+
+        if (fish == null)
+        {
+            Debug.LogError("Fish object spawned doesn't have a Fish component");
+        }
+
+        AddFish(fish);
+    }
+
+    private void AddFish(Fish fish)
+    {
+        _fishes.Add(fish);
+        fish.FishDied += OnFishDied;
+    }
+
+    private void RemoveFish(Fish fish)
+    {
+        fish.FishDied -= OnFishDied;
+        _fishes.Remove(fish);
+    }
+    #endregion
+
+    
     #region React To SpawningTimer Events
 
 
     private void OnSpawningTimerElasped()
     {
-        //TODO SPAWN FISH
-        Debug.LogWarning("Spawn Fish");
+        SpawnFish();
     }
-
     #endregion
     
     #region React To Fish Event
@@ -84,7 +137,11 @@ public class SeaFishSpawner : MonoBehaviour
     {
         SpawnedFishCollected?.Invoke(collector, scoreToAdd);
     }
-    
+
+    private void OnFishDied(Fish deadFish)
+    {
+        RemoveFish(deadFish);
+    }
     #endregion
 
     private void OnDrawGizmos()
